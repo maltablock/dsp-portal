d42d76ea523fea69052a501413280fdfe36970d4
cmichel21 <cmichel_zmeta@protonmail.com>
2019-05-30 18:22:23 +0100

    Add airHodl fetching

--- src/app/modules/profile/state/ProfileStore.ts
+++ src/app/modules/profile/state/ProfileStore.ts
@@ -1,18 +1,33 @@
 import { observable, action } from 'mobx';
-import { wallet, fetchAllRows } from 'app/shared/eos';
-import { decomposeAsset } from 'app/shared/eos/asset';
-import { DAPPSERVICES_CONTRACT } from 'app/shared/eos/constants';
-import { getTableBoundsForName, encodeName } from 'app/shared/eos/name';
-import BigNumber from 'bignumber.js';
+import { wallet, fetchRows, decomposeAsset, Symbol } from 'app/shared/eos';
+import { DAPPSERVICES_CONTRACT, DAPPHODL_CONTRACT } from 'app/shared/eos/constants';
+import { getTableBoundsForName } from 'app/shared/eos/name';
 
 // AccountInfo from eeos-transit/lib has the wrong types
 type AccountInfoFixed = {
     account_name: string;
 };
 
+type StakeInfo = {
+    account: string;
+    balance: number;
+    symbol: Symbol;
+    id: string;
+    last_reward: string;
+    last_usage: string;
+    package: string;
+    package_end: string;
+    package_started: string;
+    pending_package: string;
+    provider: string;
+    quota: number;
+    service: string;
+};
+
 type DappHdlInfo = {
     balance: number;
-    vestingLeftInSeconds: number;
+    allocation: number;
+    staked: number;
     claimed: boolean;
 };
 
@@ -23,10 +38,16 @@ type DappInfo = {
 type AccountBalanceRow = {
     balance: string;
 };
+type AccountHodlRow = {
+    balance: string;
+    allocation: string;
+    staked: string;
+    claimed: number;
+};
 
 class ProfileStore {
     @observable accountInfo?: AccountInfoFixed;
-    @observable stakes;
+    @observable stakes?: StakeInfo[];
     @observable dappHdlInfo?: DappHdlInfo;
     @observable dappInfo?: DappInfo;
 
@@ -35,7 +56,7 @@ class ProfileStore {
             await wallet.connect();
             // last Scatter login is saved, need to remove to be able to login again
             try {
-              await wallet.logout();
+                await wallet.logout();
             } catch {}
             const accountInfo = (await wallet.login()) as unknown;
             this.accountInfo = accountInfo as AccountInfoFixed;
@@ -49,14 +70,16 @@ class ProfileStore {
         if (!this.accountInfo) return;
 
         try {
-            let result = await fetchAllRows<AccountBalanceRow>({
+            const dappInfoResult = await fetchRows<AccountBalanceRow>({
                 code: DAPPSERVICES_CONTRACT,
                 scope: this.accountInfo.account_name,
                 table: `accounts`,
             });
-            this.dappInfo = {
-                unstakedBalance: decomposeAsset(result[0].balance).amount,
-            };
+            if (dappInfoResult[0]) {
+                this.dappInfo = {
+                    unstakedBalance: decomposeAsset(dappInfoResult[0].balance).amount,
+                };
+            }
 
             // by_account_service consists of 128 bit: 64 bit encoded name, 64 bit encoded service
             // https://github.com/liquidapps-io/zeus-dapp-network/blob/9f0fd5d8cff78d7f429a6284aedeb23f45f21263/dapp-services/contracts/eos/dappservices/dappservices.cpp#L116
@@ -64,24 +87,48 @@ class ProfileStore {
             const servicePart = `0`.repeat(16);
             nameBounds.lower_bound = `0x${servicePart}${nameBounds.lower_bound}`;
             nameBounds.upper_bound = `0x${servicePart}${nameBounds.upper_bound}`;
-            result = await fetchAllRows(
-                {
-                    code: DAPPSERVICES_CONTRACT,
-                    scope: `DAPP`,
-                    table: `accountext`,
-                    index_position: `3`, // &accountext::by_account_service
-                    key_type: `i128`,
-                    lower_bound: `${nameBounds.lower_bound}`,
-                    upper_bound: `${nameBounds.upper_bound}`,
-                    limit: 10,
-                },
-                ``,
-            );
-            console.log(result);
+            const stakesResult = await fetchRows<any>({
+                code: DAPPSERVICES_CONTRACT,
+                scope: `DAPP`,
+                table: `accountext`,
+                index_position: `3`, // &accountext::by_account_service
+                key_type: `i128`,
+                lower_bound: `${nameBounds.lower_bound}`,
+                upper_bound: `${nameBounds.upper_bound}`,
+            });
+            this.stakes = stakesResult.map(stake => {
+                const { amount: balance, symbol } = decomposeAsset(stake.balance);
+                const { amount: quota } = decomposeAsset(stake.quota);
+                return {
+                    ...stake,
+                    balance,
+                    symbol,
+                    quota,
+                };
+            });
+
+            const dappHodlResult = await fetchRows<AccountHodlRow>({
+                code: DAPPHODL_CONTRACT,
+                scope: this.accountInfo.account_name,
+                table: `accounts`,
+            });
+            // some accounts will not have received the airHODL
+            if (dappHodlResult[0]) {
+                this.dappHdlInfo = {
+                    balance: decomposeAsset(dappHodlResult[0].balance).amount,
+                    allocation: decomposeAsset(dappHodlResult[0].allocation).amount,
+                    staked: decomposeAsset(dappHodlResult[0].staked).amount,
+                    claimed: Boolean(dappHodlResult[0].claimed),
+                };
+            }
         } catch (error) {
             console.error(error.message);
         }
     };
+
+    get vestingEndDate() {
+        return `?`;
+    }
 }
 
 export default ProfileStore;
--- src/app/shared/eos/constants.ts
+++ src/app/shared/eos/constants.ts
@@ -1,6 +1,7 @@
 import { Symbol } from './asset'
 
 export const DAPPSERVICES_CONTRACT = `dappservices`;
+export const DAPPHODL_CONTRACT = `dappairhodl1`;
 export const DAPP_SYMBOL: Symbol = {
     symbolCode: `DAPP`,
     precision: 4,
