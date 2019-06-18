c9078efd59a5997b39db0b32ddb38d1364df9afb
Sergey Ulyashev <ulyashev@gmail.com>
2019-06-18 16:52:33 +0300

    Fill input with staked/unstaked dapp(hdl) values when clicked on label

--- src/app/modules/packages/components/DappPackageCard.tsx
+++ src/app/modules/packages/components/DappPackageCard.tsx
@@ -7,6 +7,8 @@ import { DialogStore } from 'app/modules/dialogs';
 import { stakeTransaction } from 'app/modules/transactions/logic/transactions';
 import TransactionStakeSuccess from 'app/modules/transactions/components/TransactionStakeSuccess';
 import TransactionStakePending from 'app/modules/transactions/components/TransactionStakePending';
+import { formatAsset } from 'app/shared/eos';
+import { DAPP_SYMBOL, DAPPHODL_SYMBOL } from 'app/shared/eos/constants';
 
 type Props = {
   dappPackage: DappPackage;
@@ -34,7 +36,7 @@ const DappPackageCard = ({ dappPackage, dialogStore }: Props) => {
       contentPending: <TransactionStakePending {...stakePayload} />,
       performTransaction: async () => {
         const result = await stakeTransaction(stakePayload);
-        await p.packageStore.rootStore.profileStore.fetchInfo()
+        await p.packageStore.rootStore.profileStore.fetchInfo();
         return result;
       },
       onClose: () => {
@@ -43,17 +45,39 @@ const DappPackageCard = ({ dappPackage, dialogStore }: Props) => {
     });
   };
 
+  const { stakingBalanceFromSelf } = p;
+  const { unstakedDappAmount, unstakedDappHdlAmount } = p.packageStore.rootStore.profileStore;
+
+  const unstakedDapp = formatAsset(
+    { amount: unstakedDappAmount, symbol: DAPP_SYMBOL },
+    { withSymbol: false },
+  );
+  const unstakedDapphdl = formatAsset(
+    { amount: unstakedDappHdlAmount, symbol: DAPPHODL_SYMBOL },
+    { withSymbol: false },
+  );
+
+  const details = [
+    [
+      { label: 'Quota', value: p.data.quota },
+      { label: 'Min Stake', value: p.data.min_stake_quantity },
+    ],
+  ];
+
+  if (stakingBalanceFromSelf) {
+    details[0].push({
+      label: 'Amount Staked:',
+      value: formatAsset({ amount: stakingBalanceFromSelf, symbol: DAPP_SYMBOL }),
+    });
+  }
+
+  details[0].push({ label: 'Unstake time', value: p.unstakeTimeText });
+
   return (
     <PackageCard
       package={dappPackage}
       showStakingIcon={dappPackage.isStakedToByUser}
-      details={[
-        [
-          { label: 'Quota', value: p.data.quota },
-          { label: 'Min Stake', value: p.data.min_stake_quantity },
-          { label: 'Unstake time', value: p.unstakeTimeText },
-        ],
-      ]}
+      details={details}
       input={{
         placeholder: `Stake Amount ${p.data.min_stake_quantity.split(` `)[0]}`,
       }}
@@ -63,6 +87,18 @@ const DappPackageCard = ({ dappPackage, dialogStore }: Props) => {
       }}
       stakedDappAmount={p.stakingBalanceFromSelf}
       stakedDappHdlAmount={p.stakingBalanceFromSelfDappHdl}
+      dappLabelButton={{
+        text: unstakedDapp + ' Max',
+        onClick: () => {
+          p.packageStore.stakeValueDapp = unstakedDapp;
+        },
+      }}
+      dapphdlLabelButton={{
+        text: unstakedDapphdl + ' Max',
+        onClick: () => {
+          p.packageStore.stakeValueDappHdl = unstakedDapphdl;
+        },
+      }}
     />
   );
 };
--- src/app/modules/packages/components/StakedPackageCard.tsx
+++ src/app/modules/packages/components/StakedPackageCard.tsx
@@ -23,62 +23,71 @@ const formatMinUnstakePeriod = seconds => {
   return `${hours} ${postfix}`;
 };
 
-const formatUnstakeEndTime = (date:Date) => {
-  const diffSeconds = differenceInSeconds(date, new Date())
-  const t = secondsToTimeObject(diffSeconds)
-  let timeString = ``
-  if(t.days > 0) timeString += `${t.days}D: `
-  if(t.hours > 0) timeString += `${t.hours}H: `
-  timeString += `${t.minutes}M`
-  return timeString
+const formatUnstakeEndTime = (date: Date) => {
+  const diffSeconds = differenceInSeconds(date, new Date());
+  const t = secondsToTimeObject(diffSeconds);
+  let timeString = ``;
+  if (t.days > 0) timeString += `${t.days}D: `;
+  if (t.hours > 0) timeString += `${t.hours}H: `;
+  timeString += `${t.minutes}M`;
+  return timeString;
 };
 
 const getCardDetails = (p: StakedPackage) => {
-  const details = [[
-    {
-      label: 'Quota:',
-      value: formatAsset({ amount: p.quotaNumber, symbol: QUOTA_SYMBOL }),
-    },
-    {
-      label: 'Amount Staked:',
-      value: formatAsset({ amount: p.data.balance, symbol: DAPP_SYMBOL }),
-    },
-    {
-      label: 'Min Stake:',
-      value: formatAsset({ amount: p.minStakeNumber, symbol: DAPP_SYMBOL }),
-    },
-  ]];
-
-  const canUnstakeMore = p.refundFromSelfAmount + p.refundFromSelfDappHdlAmount < p.stakingBalanceFromSelf + p.stakingBalanceFromSelfDappHdl
-  if(canUnstakeMore) {
-    details[0].push(
+  const details = [
+    [
       {
-        label: 'Min Unstake Time:',
-        value: formatMinUnstakePeriod(p.minUnstakePeriod),
-      })
+        label: 'Quota:',
+        value: formatAsset({ amount: p.quotaNumber, symbol: QUOTA_SYMBOL }),
+      },
+      {
+        label: 'Amount Staked:',
+        value: formatAsset({ amount: p.data.balance, symbol: DAPP_SYMBOL }),
+      },
+      {
+        label: 'Min Stake:',
+        value: formatAsset({ amount: p.minStakeNumber, symbol: DAPP_SYMBOL }),
+      },
+    ],
+  ];
+
+  const canUnstakeMore =
+    p.refundFromSelfAmount + p.refundFromSelfDappHdlAmount <
+    p.stakingBalanceFromSelf + p.stakingBalanceFromSelfDappHdl;
+  if (canUnstakeMore) {
+    details[0].push({
+      label: 'Min Unstake Time:',
+      value: formatMinUnstakePeriod(p.minUnstakePeriod),
+    });
   }
 
-  if(p.refundFromSelf) {
-    details.push([{
-      label: 'Amount Unstaking:',
-      value: formatAsset({ amount: p.refundFromSelf.amount, symbol: DAPP_SYMBOL }),
-    }, {
-      label: 'Time Remaining:',
-      value: formatUnstakeEndTime(p.refundFromSelf.unstake_time),
-    }])
+  if (p.refundFromSelf) {
+    details.push([
+      {
+        label: 'Amount Unstaking:',
+        value: formatAsset({ amount: p.refundFromSelf.amount, symbol: DAPP_SYMBOL }),
+      },
+      {
+        label: 'Time Remaining:',
+        value: formatUnstakeEndTime(p.refundFromSelf.unstake_time),
+      },
+    ]);
   }
 
-  if(p.refundFromSelfDappHdl) {
-    details.push([{
-      label: 'Amount Unstaking:',
-      value: formatAsset({ amount: p.refundFromSelfDappHdl.amount, symbol: DAPPHODL_SYMBOL }),
-    }, {
-      label: 'Time Remaining:',
-      value: formatUnstakeEndTime(p.refundFromSelfDappHdl.unstake_time),
-    }])
+  if (p.refundFromSelfDappHdl) {
+    details.push([
+      {
+        label: 'Amount Unstaking:',
+        value: formatAsset({ amount: p.refundFromSelfDappHdl.amount, symbol: DAPPHODL_SYMBOL }),
+      },
+      {
+        label: 'Time Remaining:',
+        value: formatUnstakeEndTime(p.refundFromSelfDappHdl.unstake_time),
+      },
+    ]);
   }
 
-  return details
+  return details;
 };
 
 const StakedPackageCard = ({ stakedPackage, dialogStore }: Props) => {
@@ -102,7 +111,7 @@ const StakedPackageCard = ({ stakedPackage, dialogStore }: Props) => {
       contentPending: <TransactionUnstakePending {...unstakePayload} />,
       performTransaction: async () => {
         const result = await unstakeTransaction(unstakePayload);
-        await p.packageStore.rootStore.profileStore.fetchInfo()
+        await p.packageStore.rootStore.profileStore.fetchInfo();
         return result;
       },
       onClose: () => {
@@ -110,6 +119,16 @@ const StakedPackageCard = ({ stakedPackage, dialogStore }: Props) => {
       },
     });
   };
+
+  const stakedDapp = formatAsset(
+    { amount: p.stakingBalanceFromSelf, symbol: DAPP_SYMBOL },
+    { withSymbol: false },
+  );
+
+  const stakedDapphdl = formatAsset(
+    { amount: p.stakingBalanceFromSelfDappHdl, symbol: DAPPHODL_SYMBOL },
+    { withSymbol: false },
+  );
 
   return (
     <PackageCard
@@ -125,6 +144,18 @@ const StakedPackageCard = ({ stakedPackage, dialogStore }: Props) => {
       deprecated={p.isDeprecated}
       stakedDappAmount={p.stakingBalanceFromSelf}
       stakedDappHdlAmount={p.stakingBalanceFromSelfDappHdl}
+      dappLabelButton={{
+        text: stakedDapp + ' Max',
+        onClick: () => {
+          p.packageStore.stakeValueDapp = stakedDapp;
+        },
+      }}
+      dapphdlLabelButton={{
+        text: stakedDapphdl + ' Max',
+        onClick: () => {
+          p.packageStore.stakeValueDappHdl = stakedDapphdl;
+        },
+      }}
     />
   );
 };
