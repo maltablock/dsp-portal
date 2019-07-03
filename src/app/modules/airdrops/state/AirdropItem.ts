12f356be57f3bf6ef5b7b840044d9e61e3278981
cmichel21 <cmichel_zmeta@protonmail.com>
2019-07-03 16:05:07 +0300

    Implement vAirdrop cards

--- src/app/modules/airdrops/state/AirdropItem.ts
+++ src/app/modules/airdrops/state/AirdropItem.ts
@@ -1,5 +1,4 @@
-import { computed, observable } from 'mobx';
-import { decomposeAsset } from 'app/shared/eos';
+import { computed, observable, action } from 'mobx';
 
 export type AirdropItemData = {
   issuer: string;
@@ -10,9 +9,32 @@ export type AirdropItemData = {
 
 export default class AirdropItem {
   @observable data: AirdropItemData;
+  @observable balance: number = 0;
+  @observable claimed: boolean = false;
 
   constructor(data: AirdropItemData) {
     this.data = data;
+  }
+
+  @action resetBalance() {
+    this.balance = 0;
+    this.claimed = false;
+  }
+
+  @action async fetchBalance(account: string) {
+    this.resetBalance();
+
+    this.balance = Math.trunc(Math.random() * 1234);
+    this.claimed = Math.random() < 0.5;
+  }
+
+  @computed get tokenSymbol() {
+    const [precision, name] = this.data.token.split(`,`);
+
+    return {
+      symbolCode: name,
+      precision: Number.parseInt(precision),
+    };
   }
 
   @computed get tokenName() {
@@ -22,4 +44,14 @@ export default class AirdropItem {
   @computed get tokenContract() {
     return this.data.issuer;
   }
+
+  @computed get endDate() {
+    return `TBD`;
+  }
+
+  @computed get bytesRequired() {
+    return 241;
+  }
+
+  @action async claim() {}
 }
