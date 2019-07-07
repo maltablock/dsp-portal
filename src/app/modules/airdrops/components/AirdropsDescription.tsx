af5810485c5d290b8b3d7da289215d961007ccb5
cmichel21 <cmichel_zmeta@protonmail.com>
2019-07-07 14:03:19 +0300

    Rename vAirdrops to LiquidAirdrops

--- src/app/modules/airdrops/components/AirdropsDescription.tsx
+++ src/app/modules/airdrops/components/AirdropsDescription.tsx
@@ -80,7 +80,7 @@ const AirdropsDescription: React.FC<Props> = ({ airdropStore }: Props) => {
 
   return (
     <CardWrapper>
-      <Title>vAirdrops</Title>
+      <Title>LiquidAirdrops</Title>
       <Message>
         vAirdrop is a new breakthrough technique for distributing tokens on a massive scale using
         Web Oracle Service through a DAPP Service Provider (DSP). vAirdrop can be done at a fraction
