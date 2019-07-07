83604d755c365d5a8791da2b62eb8d03fe14263c
cmichel21 <cmichel_zmeta@protonmail.com>
2019-07-07 16:26:31 +0300

    Fix account liquidairdrops fetching when no entry exists + smaller improvements

--- src/app/modules/transactions/components/TransactionVClaimSuccess.tsx
+++ src/app/modules/transactions/components/TransactionVClaimSuccess.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { VClaimPayload } from '../logic/transactions';
-import { AmountText, ContentInfo } from './TransactionStyles';
+import { AmountText, ContentInfo, HighlightedText2 } from './TransactionStyles';
 
 const TransactionVClaimSuccess = (payload: VClaimPayload) => {
   return (
     <React.Fragment>
       <ContentInfo>
-        Claimed <AmountText>{payload.symbol.split(`,`)[1]}</AmountText> tokens.
+        Claimed <AmountText>{payload.symbol.split(`,`)[1]}</AmountText> tokens for account <HighlightedText2>{payload.accountToClaimFor}</HighlightedText2>.
       </ContentInfo>
     </React.Fragment>
   );
