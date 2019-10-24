4611a276fefe6bb0e978d4a0bf0574addac2a404
sergey.ulyashev <ulyashev@gmail.com>
2019-10-24 04:38:08 +0300

    Fix trailing slash issue for LiquidAirdrops route

--- src/app/root/components/Content.tsx
+++ src/app/root/components/Content.tsx
@@ -8,6 +8,13 @@ import { ROUTES } from '../constants/routes';
 const PageContent = () => (
   <Switch>
     <Route path={ROUTES.MAIN} exact component={DspServicesContent} />
+
+    {/*
+      Temporary fix to handle trailing slash for `LIQUID_AIRDROPS` route.
+      TODO: Remove this redirect when it will be fixed on the server-side.
+    */}
+    <Redirect exact strict from={ROUTES.LIQUID_AIRDROPS + '/'} to={ROUTES.LIQUID_AIRDROPS} />
+
     <Route path={ROUTES.LIQUID_AIRDROPS} exact component={AirdropsContent} />
     <Redirect to={ROUTES.MAIN} />
   </Switch>
